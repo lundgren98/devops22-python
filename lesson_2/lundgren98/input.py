d96e3971cd7bbb84cd8cf2180012763be4f2c828
Alexander Lundgren <alexander.lundgren@yh.nackademin.se>
2022-09-01 09:02:18 +0200

    input exercise

---
+++ lesson_2/lundgren98/input.py
@@ -0,0 +1,15 @@
+
+
+def inputing():
+    s = input()
+    print(s)
+    i = int(input())
+    print(i * 2)
+    s = input()
+    print(s + s)
+    f = float(input())
+    print(f / 3.5)
+
+
+if __name__ == "__main__":
+    inputing()
