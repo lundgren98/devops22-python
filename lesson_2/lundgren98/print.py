05f24ee87939c5ae563cae347716ce1c67a206c9
Alexander Lundgren <alexander.lundgren@yh.nackademin.se>
2022-09-01 09:01:16 +0200

    printing exercise

---
+++ lesson_2/lundgren98/print.py
@@ -0,0 +1,17 @@
+
+
+def printing():
+    i = 2
+    f = 1.3
+    s = "Hello World!"
+    b = True
+    n = None
+    print(i)
+    print(f)
+    print(s)
+    print(b)
+    print(n)
+    print(f"{i}, {f}, {s}, {b}, {n}")
+
+if __name__ == "__main__":
+    printing()
