144bd829abaa3390fd7d7857fc8f7e1961660bb8
kockoklock <pbjtfy@gmail.com>
2022-09-02 20:38:29 +0200

    redid shoesize.py

--- lesson_3/lundgren98/shoesize.py
+++ lesson_3/lundgren98/shoesize.py
@@ -1,72 +1,34 @@
-names = []
-ages  = []
-sizes = []
-res = input("Please enter name for person 1 ")
-names.append(res.title())
-res = input("Please enter age for person 1 ")
-ages.append(int(res))
-res = input("Please enter shoe size for person 1 ")
-sizes.append(int(res))
-res = input("Please enter name for person 2 ")
-names.append(res.title())
-res = input("Please enter age for person 2 ")
-ages.append(int(res))
-res = input("Please enter shoe size for person 2 ")
-sizes.append(int(res))
-res = input("Please enter name for person 3 ")
-names.append(res.title())
-res = input("Please enter age for person 3 ")
-ages.append(int(res))
-res = input("Please enter shoe size for person 3 ")
-sizes.append(int(res))
-
-age_to_name = {
-        str(ages[0]): names[0],
-        str(ages[1]): names[1],
-        str(ages[2]): names[2]
+people = [
+        {
+            "name": input("Please enter name for person 1: "),
+            "age": input("Please enter age for person 1: "),
+            "size": input("Please enter shoe size for person 1: "),
+        },
+        {
+            "name": input("Please enter name for person 2: "),
+            "age": input("Please enter age for person 2: "),
+            "size": input("Please enter shoe size for person 2: "),
+        },
+        {
+            "name": input("Please enter name for person 3: "),
+            "age": input("Please enter age for person 3: "),
+            "size": input("Please enter shoe size for person 3: "),
         }
-age_to_size = {
-        str(ages[0]): str(sizes[0]),
-        str(ages[1]): str(sizes[1]),
-        str(ages[2]): str(sizes[2])
-        }
-name_to_size = {
-        names[0]: str(sizes[0]),
-        names[1]: str(sizes[1]),
-        names[2]: str(sizes[2])
-        }
-eldest_age = str(sorted(ages)[-1])
-eldest_name = age_to_name[eldest_age]
-eldest_size = age_to_size[eldest_age]
+        ]
 
-size_to_age = {v: k for k, v in age_to_size.items()}
-biggest_size = str(sorted(sizes)[1])
-biggest_age = size_to_age[biggest_size]
-biggest_name = age_to_name[biggest_age]
+# Find the oldest person
+eldest = sorted(people, key=lambda person : person["age"])[-1]
+# Find the median shoe sized person
+median_size = sorted(people, key=lambda person : person["size"])[1]
 
-name_to_age = {v: k for k, v in age_to_name.items()}
-size_to_name = {v: k for k, v in age_to_name.items()}
-
-commands = {
-        "name": [name_to_age, name_to_size],
-        "age": [age_to_name, age_to_size],
-        "size": [size_to_age, size_to_name]
-        }
-
-present_order = {
-        "name": ["age", "size"],
-        "age": ["name", "size"],
-        "size": ["age", "name"]
-        }
-
-print(f'The oldest person is {eldest_name} who has shoe size {eldest_size}')
-print(f'The person with median shoe size is {biggest_name} who is {biggest_age} years old')
+print(f'The oldest person is {eldest["name"]} who has shoe size {eldest["size"]}')
+print(f'The person with median shoe size is {median_size["name"]} who is {eldest["age"]} years old')
 res = input('Please enter search value, name, age or size followed by value: ')
-the_input = res.split()
-my_key = the_input[0]
-my_value = the_input[1]
+com, val = res.split()
 
-print('Found Person')
-print(f'{my_key}: {my_value}')
-print(f'{present_order[my_key][0]}: {commands[my_key][0][my_value]}')
-print(f'{present_order[my_key][1]}: {commands[my_key][1][my_value]}')
+# Case insensitive
+com = com.lower()
+val = val.title()
+
+searched = sorted(people, key=lambda person : person[com] == val)[-1]
+print(f'Found person\n\tName: {searched["name"]}\n\tAge: {searched["age"]}\n\tSize: {searched["size"]}')
